ed125a0a41fd9831eb22a0fc59549fcd4d797382
tanpabatasbro <115479649+tanpabatasbro@users.noreply.github.com>
2022-11-04 20:09:38 +0700

    Add files via upload

---
+++ Array.py
@@ -0,0 +1,16 @@
+angka = [1,2,3,4,5,6,7,8,9,10]
+print(angka)
+print()
+#menghapus elemen ke-3 dari list
+angka = [1,2,3,4,5,6,7,8,9,10]
+angka.remove(3)
+print(angka)
+print()
+#menambahkan angka 5 kedalam elemen ke-3
+angka = [1,2,3,4,5,6,7,8,9,10]
+angka.insert(3,5)
+print(angka)
+print()
+#memilih untuk hanya menampilkan beberapa dari list
+angka = [1,2,3,4,5,6,7,8,9,10]
+print(angka[7:])#hanyaa menampilkan 3 angka terakhir dari list
